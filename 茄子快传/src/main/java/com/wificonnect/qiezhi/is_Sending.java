6fcb770ae5bd4a94f6ece0ee621ced7c92acb8e1
Administrator <957250254@qq.com>
2016-09-08 15:22:38 +0800

    提交

--- 茄子快传/src/main/java/com/wificonnect/qiezhi/is_Sending.java
+++ 茄子快传/src/main/java/com/wificonnect/qiezhi/is_Sending.java
@@ -48,6 +48,7 @@ public class is_Sending extends Activity implements View.OnClickListener {
 
     private void assignViews() {
 
+
         research_button = (Button) findViewById(R.id.research_button);
         research_button.setOnClickListener(this);
         View title = findViewById(R.id.title);
